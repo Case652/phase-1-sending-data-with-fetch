d9e88c9b1df43356ffeaa7faf3b912d2c713afba
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-09-25 08:33:17 -0500

    submited

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+/*const formData = {
+    name: " ",
+    email: " ",
+};
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+};*/
+
+const data = ((r) => r.json())
+
+
+function submitData(name,email) {
+    return fetch(`http://localhost:3000/users`,{
+        method:"POST",
+        headers:{
+            "Content-Type": `application/json`,
+            "Accept": `application/json`,
+        },
+        body: JSON.stringify({name, email,})
+    })
+    .then(data)
+    .then((errorObject) => 
+        document.body.innerHTML = errorObject.id)
+    .catch(function(error) {
+            alert("Bad thing");
+            console.log(error.message);
+            document.body.innerHTML = error.message
+})
+};
